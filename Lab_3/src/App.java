ba35432c5e8bf6a14ac8191c3f58db8b4ab864bd
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-03 16:23:29 +0300

    Lab3 added input from cmd

--- Lab_3/src/App.java
+++ Lab_3/src/App.java
@@ -3,19 +3,23 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        List<Integer> numbers = ReadFile.readDataFromFile("F:\\Desktop\\java\\CPT-course\\Lab_3\\2.bin");
+        if(args.length == 1) {
+            List<Integer> numbers = ReadFile.readDataFromFile(args[0]);
 
-        int dynamicSignalRange = DigitalSignalProcessing.getDynamicSignalRange(numbers);
-        int signalEnergy = DigitalSignalProcessing.getSignalEnergy(numbers);
-        double averageSignalPower = DigitalSignalProcessing.getAverageSignalPower(numbers);
-        double valueOfSignalReadings = DigitalSignalProcessing.getAverageValueOfSignalReadings(numbers);
-        double varianceOfSignalReadings = DigitalSignalProcessing.getVarianceOfSignalReadings(numbers);
+            int dynamicSignalRange = DigitalSignalProcessing.getDynamicSignalRange(numbers);
+            int signalEnergy = DigitalSignalProcessing.getSignalEnergy(numbers);
+            double averageSignalPower = DigitalSignalProcessing.getAverageSignalPower(numbers);
+            double valueOfSignalReadings = DigitalSignalProcessing.getAverageValueOfSignalReadings(numbers);
+            double varianceOfSignalReadings = DigitalSignalProcessing.getVarianceOfSignalReadings(numbers);
 
-        System.out.println("Dynamic signal range: " + dynamicSignalRange);
-        System.out.println("Signal energy: " + signalEnergy);
-        System.out.println("Average signal power: " + averageSignalPower);
-        System.out.println("Value of signal readings: " + valueOfSignalReadings);
-        System.out.println("Variance of signal readings: " + varianceOfSignalReadings);
+            System.out.println("Dynamic signal range: " + dynamicSignalRange);
+            System.out.println("Signal energy: " + signalEnergy);
+            System.out.println("Average signal power: " + averageSignalPower);
+            System.out.println("Value of signal readings: " + valueOfSignalReadings);
+            System.out.println("Variance of signal readings: " + varianceOfSignalReadings);
+        } else {
+            System.err.println("Incorrect input data");
+        }
     }
 
 }
