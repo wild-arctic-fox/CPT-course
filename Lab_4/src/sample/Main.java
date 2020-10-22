eea132d77f6614150f5787e308823604f058a5ae
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-10-22 10:40:11 +0300

    Lab_4 final version

--- Lab_4/src/sample/Main.java
+++ Lab_4/src/sample/Main.java
@@ -1,8 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
